adccacd35492b4d03adb91f356e67975094aa6ec
Martin Johannesson <mjohannesson@gmail.com>
2014-01-06 00:22:29 +0100

    Fixed target conditionals.

--- ImPro/shaders.h
+++ ImPro/shaders.h
@@ -5,11 +5,11 @@
 /* Convenience macro for writing shaders in C code files. */
 #define SHADER_NSSTRING(x) @ #x
 
-#ifdef TARGET_OS_MAC
+#ifdef TARGET_OS_MAC && !TARGET_OS_IPHONE
 #define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 // --- OS X shader goes here
 #define FRAGMENT_SHADER_FILE(x) @ #x " OS X"
-#elif defined(TARGET_OS_IPHONE)
+#elif TARGET_OS_IPHONE
 // --- iOS shader goes here
 #define FRAGMENT_SHADER_FILE(x) @ #x " iOS"
 #else
